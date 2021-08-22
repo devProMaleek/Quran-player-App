b6a870bb25f4d935ab93e0d747b48a4d0cbe7644
devProMaleek <milikiadbay@gmail.com>
2021-08-22 18:02:29 +0100

    Done with the function to set surah

--- app.js
+++ app.js
@@ -62,3 +62,73 @@ const backToQuranPlayer = document.querySelector(".playlist .back-btn");
 backToQuranPlayer.addEventListener("click", () => {
   playlistSection.classList.remove('active');
 });
+
+// Music
+
+// Set the global variable.
+let currentMusic = 0;
+
+const music = document.querySelector('#audio-source')
+const seekBar = document.querySelector('.music-seek-bar')
+const songName = document.querySelector('.current-song-name')
+const artistName = document.querySelector('.artist-name')
+const coverImage = document.querySelector('.cover')
+const currentMusicTime = document.querySelector('.current-time')
+const musicDuration = document.querySelector('.duration')
+
+const queue = [...document.querySelectorAll('.queue')]
+
+
+// Select all buttons
+
+const forwardBtn = document.querySelector('i.fa-forward');
+const backwardBtn = document.querySelector('i.fa-backward')
+const playBtn = document.querySelector('i.fa-play')
+const pauseBtn = document.querySelector('i.fa-pause')
+const repeatBtn = document.querySelector('span.fa-redo')
+const volumeBtn = document.querySelector('span.fa-volume-up')
+const volumeSlider = document.querySelector('.volume-slider')
+
+// Function for setting up music
+
+const setMusic = (i) => {
+    seekBar.value = 0;
+    let song = songs[i]
+    currentMusic = i;
+
+    music.src = song.path
+
+    songName.innerHTML = song.name
+    artistName.innerHTML = song.artist
+    coverImage.src = song.cover
+
+    setTimeout(() => {
+        seekBar.max = music.duration;
+        musicDuration.innerHTML = formatTime(music.duration) 
+    }, 300)
+    currentMusicTime.innerHTML = "00:00"    
+    queue.forEach(item => item.classList.remove('active'))
+    queue[currentMusic].classList.add('active')
+}
+
+setMusic(1)
+
+// Format duration in 00:00:00 format
+
+const formatTime = (time) => {
+    // Format Hours
+    let hrs = Math.floor(time / 60 / 60)
+    // Format Minutes
+    let min = Math.floor(time / 60 % 60)
+    if(min < 10) {
+        min = `0` + min;
+    }
+
+    // Format Seconds
+    let sec = Math.floor(time % 60)
+    if (sec < 10) {
+        sec = `0` + sec
+    }
+
+    return `${hrs}:${min}:${sec}`
+}
